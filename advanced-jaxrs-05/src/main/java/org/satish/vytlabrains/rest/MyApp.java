5231b7b993ff1b261c58312633ff40887461f9b2
satish-vytla <vytla.satish@gmail.com>
2016-06-01 14:09:29 -0400

    JAXRS

---
+++ advanced-jaxrs-05/src/main/java/org/satish/vytlabrains/rest/MyApp.java
@@ -0,0 +1,8 @@
+package org.satish.vytlabrains.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+@ApplicationPath("webapi")
+public class MyApp extends Application{
+
+}
